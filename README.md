7f51a24129aa045c918edd8966896fb87b23a146
Eli Judah <elijudah@gmail.com>
2019-04-04 17:28:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Slow
+
+#### Takes the output of commands like `kubectl logs` and `stern`, and outputs them ...slowly
+
+### Build
+
+````sh
+git clone git@github.dns.ad.zopa.com:zopaUK/slow.git
+cd slow
+go build slow.go
+mv slow /usr/local/bin```
+
+### Flags
+`-wait int`
+time to wait in seconds between printing output
+
+### Examples
+`kubectl logs <some nginx pod> | slow`
+
+`stern <some nginx pod> | slow -wait 2`
+````
---
+++ slow.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+
+	waitTime := flag.Int("wait", 1, "time to wait in seconds between printing output")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	go func() {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+			time.Sleep(time.Duration(*waitTime) * time.Second)
+		}
+	}()
+	<-c
+}
